22b79270314a6605c3398308a2a2ce6b2870c7e3
Mohammad Murad <muhammad.talal.murad@gmail.com>
2024-09-29 13:54:18 -0700

    about , contact added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mt-28 font-sans">
+        <p className="text-ellipsis basis-3/4 sm:text-7xl text-4xl font-bold tracking-tight">
+        <span className="text-gray-700">OBAIDAH</span> <span className="text-[#06b6d4]">Online Shop, </span>
+          <span className="text-gray-500">
+            based in Amman, Jordan. help clients to Find their needs through A
+            wide content online shop.
+          </span>
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default About;
