b8ae0c0e0adec532db110b89c5834b39635465c8
Mohammad Murad <muhammad.talal.murad@gmail.com>
2024-10-17 11:34:25 -0700

    drawer fixed

--- src/components/helpers/navbar helpers/cart/cart.js
+++ src/components/helpers/navbar helpers/cart/cart.js
@@ -3,19 +3,25 @@ import { IoCartOutline } from "react-icons/io5";
 
 import CartDrawer from "../cart/cartDrawer";
 export default function Cart() {
-  const [show, setShow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const openDrawerHandler = () => setShow((s) => !s);
+  function closeButtonHandler() {
+    setIsOpen(false);
+  }
 
   return (
     <div className="flex sm:flex-none flex-1 items-center justify-center sm:items-stretch">
       <div className="flex flex-shrink-0 items-center justify-items-start">
         <IoCartOutline
-          onClick={openDrawerHandler}
+          onClick={() => setIsOpen(true)}
           size={30}
           className="text-lg font-sans tracking-wide text-gray-400 hover:text-gray-800"
         />
-        <CartDrawer show={show} />
+        <CartDrawer
+          open={isOpen}
+          onClose={setIsOpen}
+          closeButton={closeButtonHandler}
+        />
       </div>
     </div>
   );
--- src/components/helpers/navbar helpers/cart/cartDrawer.js
+++ src/components/helpers/navbar helpers/cart/cartDrawer.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogBackdrop,
@@ -10,17 +9,13 @@ import {
 } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function CartDrawer({ show }) {
-  const [open, setOpen] = useState(true);
-
-    useEffect(() => {
-      setOpen(true)
-      
-    }, [show])
-
+export default function CartDrawer({ open, onClose, closeButton }) {
+  function closeDrawer() {
+    closeButton();
+  }
 
   return (
-    <Dialog open={open} onClose={setOpen} className="relative z-10">
+    <Dialog open={open} onClose={onClose} className="relative z-10">
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity duration-500 ease-in-out data-[closed]:opacity-0"
@@ -37,7 +32,7 @@ export default function CartDrawer({ show }) {
                 <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 duration-500 ease-in-out data-[closed]:opacity-0 sm:-ml-10 sm:pr-4">
                   <button
                     type="button"
-                    onClick={() => setOpen(false)}
+                    onClick={() => closeDrawer(false)}
                     className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
                   >
                     <span className="absolute -inset-2.5" />
