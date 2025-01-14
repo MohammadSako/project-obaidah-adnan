4edef23aa4b77084b1bdc4ac005d0b379273e972
MohammadSako <muhammad.talal.murad@gmail.com>
2025-01-14 15:30:24 +0300

    auth1

--- src/app/public.tsx
+++ src/app/public.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@/utils/supabase/static-props'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function PublicPage({ data }: { data?: any[] }) {
   return <pre>{data && JSON.stringify(data, null, 2)}</pre>
 }
